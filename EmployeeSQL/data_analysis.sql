e145b7aeaa36534001fca59145a4fbaf4397225f
zombimaki <andrewbperez@gmail.com>
2020-11-15 10:53:39 -0600

    dept

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -55,7 +55,6 @@ ORDER BY
 	e.emp_no,
 	d.dept_name
 	
-
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT
 	e.first_name,
@@ -70,8 +69,37 @@ AND
 order by 
 	e.last_name,
 	e.sex
+	
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+SELECT 
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM 
+	employees AS e
+	INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
+	INNER JOIN departments AS d ON (de.dept_no = d.dept_no)
+WHERE
+	d.dept_name = 'Sales'
+ORDER BY
+	e.emp_no
+	
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT 
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM 
+	employees AS e
+	INNER JOIN dept_emp AS de ON (e.emp_no = de.emp_no)
+	INNER JOIN departments AS d ON (de.dept_no = d.dept_no)
+WHERE
+	d.dept_name in ('Sales','Development')
+ORDER BY
+	d.dept_name,
+	e.emp_no
+	
+	
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
