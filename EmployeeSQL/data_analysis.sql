3447e0c6fc456354c5bf3ade75ba8f6a5a588d87
zombimaki <andrewbperez@gmail.com>
2020-11-15 10:23:03 -0600

    erd

---
+++ EmployeeSQL/data_analysis.sql
@@ -0,0 +1,40 @@
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT 
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	e.sex,
+	s.salary
+FROM 
+	employees AS e
+	INNER JOIN salaries AS s ON (e.emp_no = s.emp_no)
+ORDER BY 
+	s.salary desc, 
+	e.emp_no
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT 
+	e.first_name,
+	e.last_name,
+	e.hire_date
+FROM 
+	employees AS e
+WHERE 
+	EXTRACT(YEAR FROM e.hire_date) = 1986
+ORDER BY
+	e.hire_date, 
+	e.last_name, 
+	e.first_name
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
